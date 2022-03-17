066888cbb40261bec548bc87ad75e8796ecc2f20
Cesar <ipog71@gmail.com>
2022-03-17 11:11:36 -0600

    Ordenar alfabeticamente

---
+++ Pruebas de orden alfabetico/orden alfabetico 2.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cstdlib>
+ 
+using namespace std;
+
+int main()
+{
+    string cadena1 = "alan";
+    string cadena2 = "alannnnnn";
+    string cadena3 = "mariano";
+    if (cadena1 < cadena2)
+    {
+        cout<<cadena1<<endl;
+    }
+    else if (cadena2 < cadena1)
+    {
+        cout<<cadena2<<endl;
+    }
+    
+    
+    system("PAUSE");
+    return 0;
+}
