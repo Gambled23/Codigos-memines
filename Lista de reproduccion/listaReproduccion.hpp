67038d2051f3faa156570cda82bfc59042a7edc2
Cesar <ipog71@gmail.com>
2022-04-01 12:27:40 -0600

    creacion de archivos

---
+++ Lista de reproduccion/listaReproduccion.hpp
@@ -0,0 +1,22 @@
+#ifndef LISTAREPRODUCCION_H
+#define LISTAREPRODUCCION_H
+#include <iostream>
+#include <cstdlib>
+#pragma once
+
+using namespace std;
+
+class listaReproduccion
+{
+public:
+    listaReproduccion();
+
+private:
+
+};
+
+listaReproduccion::listaReproduccion()
+{
+
+}
+#endif
