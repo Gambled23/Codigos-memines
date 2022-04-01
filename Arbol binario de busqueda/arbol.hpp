265788534aaa073a564b338a85b8fb563b2f1488
Cesar <ipog71@gmail.com>
2022-04-01 12:20:53 -0600

    inicios arbol

--- Arbol binario de busqueda/arbol.hpp
+++ Arbol binario de busqueda/arbol.hpp
@@ -12,12 +12,30 @@ class arbol
 public:
     arbol();
     nodo *raiz;
+    void insertarElemento(nodo *, int);
 
 private:
 };
 
 arbol::arbol()
 {
-    
+    raiz = nullptr;
+}
+void arbol::insertarElemento(nodo *actual, int e)
+{
+    nodo *nuevo_nodo = new nodo(e);
+    nodo *aux = new nodo();
+    if (!raiz) //Si el arbol esta vacio
+    {
+        raiz = nuevo_nodo;
+    }
+    else if (e < raiz->dato) //Si el arbol tiene 1 o mas nodos
+    {
+        
+    }
+    else if (e > raiz->dato)
+    {
+        
+    }
 }
 #endif
